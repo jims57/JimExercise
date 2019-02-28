bf70ffc0886839b6c9b56301d767e1ef0a830a8d
jims57 <jims57@163.com>
2019-02-28 18:17:59 +0800

    Add service layer folder and its file.

---
+++ service/contact.js
@@ -0,0 +1,26 @@
+module.exports = {
+    index: async (ctx, next) => {
+        let data;
+        data = 'd1';
+
+        return data;
+    },
+    listContact: async (ctx, next) => {
+        var a = ctx;
+    },
+    postcontact: async (name, pwd) => {
+        let data;
+        data = 'd1';
+
+        return data;
+    },
+    putContact: async (ctx, next) => {
+        var a = ctx;
+    },
+    delContact: async (ctx, next) => {
+        var a = ctx;
+    },
+    allContact: async (ctx, next) => {
+        var a = ctx;
+    }
+};
