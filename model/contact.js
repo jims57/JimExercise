866b17a8a6cec1327659866f0399e884734ba583
jims57 <jims57@163.com>
2019-03-01 15:29:32 +0800

    Add models files.

--- model/contact.js
+++
@@ -1,8 +0,0 @@
-//Define model
-const Contact = sequelize.define('contact', {
-    UserID: Sequelize.INTEGER,
-    Title: Sequelize.STRING,
-    Name: Sequelize.STRING,
-    BirthDate: Sequelize.DATE,
-    IsFavorite: Sequelize.INTEGER
-});
