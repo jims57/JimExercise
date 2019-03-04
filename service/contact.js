7ce80e1b7536db02d7aaa807289f4df2420745d4
jims57 <jims57@163.com>
2019-03-04 18:45:52 +0800

    Finish ordering for columns.

--- service/contact.js
+++ service/contact.js
@@ -47,22 +47,72 @@ module.exports = {
         var pageIndex = parseInt(ctx.request.query.start); // Get page index, number from 0 to N
         var pageSize = parseInt(ctx.request.query.length); // Get page size
         var jsonString = '';
-        var sqlString = 'select (select count(*) from contact) as totalCount, c.UserID, c.Title, Name, FLOOR(DATEDIFF (NOW(), BirthDate)/365) AS Age, c.IsFavorite As FavoriteFlag, (select count(UserID) from contactdetail where contactdetail.UserID = c.UserID) as ContactDetailCount from contact c limit ' + pageIndex + ', ' + pageSize + '';
 
-        await ctx.DB.query(sqlString, { type: ctx.DB.QueryTypes.SELECT}
-        )
-        .then(contacts => {
-            var contactsString = JSON.stringify(contacts);
-            var showTotal = contacts.length; // How many record to be showed in a single page
-            var totalCount = 0;
-
-            if(showTotal >0)
-            {
-                totalCount = contacts[0].totalCount;
+        var queryStringArray = ctx.request.querystring.split('&');
+        var conditionArray = queryStringArray.filter(function(value) {
+            // Get search text
+            if(value.indexOf('search%5Bvalue%5D') == 0 ||
+               value.indexOf('order%5B0%5D%5Bcolumn%5D') == 0 ||
+               value.indexOf('order%5B0%5D%5Bdir%5D') == 0
+            ){
+                return value;
             }
+        });
 
-            jsonString = '{"draw":'+ drawID +',"recordsTotal":' + showTotal + ',"recordsFiltered":' + totalCount +',"data":'+ contactsString +'}';
-        })
+        var sortingColIndexArray = conditionArray[0].split('=');
+        var sortingColConditionArray = conditionArray[1].split('=');
+        var searchArray = conditionArray[2].split('=');
+        var sortingColIndex = parseInt(sortingColIndexArray[1]);
+        var sortingColCondition = sortingColConditionArray[1];
+        var searchText = searchArray[1];
+
+        // Get sorting column name
+        var sortingColName = 'UserID';
+        switch (sortingColIndex)
+        {
+            case 0:
+                sortingColName = 'UserID';
+            break;
+            case 1:
+                sortingColName = 'Title';
+            break;
+            case 2:
+                sortingColName = 'Name';
+            break;
+            case 3:
+                sortingColName = 'BirthDate';
+                if(sortingColCondition =='asc')
+                {
+                    sortingColCondition = 'desc';
+                }else{
+                    sortingColCondition = 'asc';
+                }
+            break;
+            case 4:
+                sortingColName = 'IsFavorite';
+            break;
+            case 5:
+                sortingColName = 'ContactDetailCount';
+            break;
+            default:
+                sortingColName = 'UserID';
+        }
+
+        var sqlString = 'select (select count(*) from contact) as totalCount, c.UserID, c.Title, Name, FLOOR(DATEDIFF (NOW(), BirthDate)/365) AS Age, c.IsFavorite As FavoriteFlag, (select count(UserID) from contactdetail where contactdetail.UserID = c.UserID) as ContactDetailCount from contact c order by c.' + sortingColName + ' ' + sortingColCondition + ' limit ' + pageIndex + ', ' + pageSize + '';
+
+        await ctx.DB.query(sqlString, { type: ctx.DB.QueryTypes.SELECT })
+                    .then(contacts => {
+                        var contactsString = JSON.stringify(contacts);
+                        var showTotal = contacts.length; // How many record to be showed in a single page
+                        var totalCount = 0;
+
+                        if(showTotal >0)
+                        {
+                            totalCount = contacts[0].totalCount;
+                        }
+
+                        jsonString = '{"draw":'+ drawID +',"recordsTotal":' + showTotal + ',"recordsFiltered":' + totalCount +',"data":'+ contactsString +'}';
+                    })
 
         ctx.response.body = jsonString;
     }
